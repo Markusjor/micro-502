82d57f71270c343b712f0160b1ef5ca0d451b05e
Markus Jørgensen <markus.dahl.jorgensen@gmail.com>
2026-03-03 16:07:40 +0100

    Exercise 0

--- controllers/main/main.py
+++ controllers/main/main.py
@@ -15,7 +15,6 @@ import time, random
 import threading
 
 exp_num = 4                    # 0: Coordinate Transformation, 1: PID Tuning, 2: Kalman Filter, 3: Motion Planning, 4: Project
-
 control_style = 'keyboard'     # 'keyboard' or 'path_planner'
 rand_env = True                # Randomise the environment
 
